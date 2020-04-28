3c1d8196b1b5808df5fcf7b275a1747e9db6c174
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-04-28 13:13:09 +0900

    場合でわけて数値が正しく表示されるように修正

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import math
 
-number = 0
-while number <= 360:
-  print(str(number)+"°")
+angle = 0
+while angle <= 360:
+  print(str(angle)+"°")
   print("sin")
-  print(round(math.sin(math.radians(number)), 3))
-  print("cos")
-  print(round(math.cos(math.radians(number)), 3))
-  print("tan")
-  if number == 90 or number == 270:
-    print("定義できません")
+  if angle == 0 or angle == 180 or angle == 360:
+    print(0)
   else:
-    print(round(math.tan(math.radians(number)), 3))
+    print(round(math.sin(math.radians(angle)), 3))
+  print("cos")
+  if angle == 90 or angle == 270:
+    print(0)
+  else:
+    print(round(math.cos(math.radians(angle)), 3))
+  print("tan")
+  if angle == 90 or angle == 270:
+    print("定義できません")
+  elif angle == 0 or angle == 180 or angle == 360:
+    print(0)
+  else:
+    print(round(math.tan(math.radians(angle)), 3))
   print("\n")
-  number += 15
+  angle += 15
