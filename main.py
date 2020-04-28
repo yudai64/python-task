eeffcae345999818a8c631b80d4667bbe39d096d
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-04-28 11:02:47 +0900

    結果が正しく表示されるように修正

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ number = 0
 while number <= 360:
   print(str(number)+"°")
   print("sin")
-  print(math.sin(math.radians(number)))
+  print(round(math.sin(math.radians(number)), 3))
   print("cos")
-  print(math.cos(math.radians(number)))
+  print(round(math.cos(math.radians(number)), 3))
   print("tan")
-  print(math.tan(math.radians(number)))
+  if number == 90 or number == 270:
+    print("定義できません")
+  else:
+    print(round(math.tan(math.radians(number)), 3))
   print("\n")
   number += 15
