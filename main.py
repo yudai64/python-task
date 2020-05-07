34a43e51ee870276aa304dfb0ab8ee09cc8cec25
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-05-07 15:25:58 +0900

    表示結果を修正

--- main.py
+++ main.py
@@ -3,22 +3,10 @@ import math
 angle = 0
 while angle <= 360:
   print(str(angle)+"°")
-  print("sin")
-  if angle == 0 or angle == 180 or angle == 360:
-    print(0)
-  else:
-    print(round(math.sin(math.radians(angle)), 3))
-  print("cos")
-  if angle == 90 or angle == 270:
-    print(0)
-  else:
-    print(round(math.cos(math.radians(angle)), 3))
-  print("tan")
-  if angle == 90 or angle == 270:
-    print("定義できません")
-  elif angle == 0 or angle == 180 or angle == 360:
-    print(0)
-  else:
-    print(round(math.tan(math.radians(angle)), 3))
+  print("sin" + str(angle) + "°= " + str(round(math.sin(math.radians(angle)), 3)))
+  # print(round(math.sin(math.radians(angle)), 3))
+  print("cos" + str(angle) + "°= " + str(round(math.cos(math.radians(angle)), 3)))
+  print("tan" +str(angle) + "°= " + str(round(math.tan(math.radians(angle)), 3)))
+  # print(round(math.tan(math.radians(angle)), 3))
   print("\n")
   angle += 15
