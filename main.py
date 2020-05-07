a90a3728016740b2e5dc720b051eaf4b6e0c735b
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-05-07 15:28:32 +0900

    不要なコメントの削除

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ angle = 0
 while angle <= 360:
   print(str(angle)+"°")
   print("sin" + str(angle) + "°= " + str(round(math.sin(math.radians(angle)), 3)))
-  # print(round(math.sin(math.radians(angle)), 3))
   print("cos" + str(angle) + "°= " + str(round(math.cos(math.radians(angle)), 3)))
   print("tan" +str(angle) + "°= " + str(round(math.tan(math.radians(angle)), 3)))
-  # print(round(math.tan(math.radians(angle)), 3))
   print("\n")
   angle += 15
